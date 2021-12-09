d56d1d7ee08f951dd2d061b3f899cdb449dc39ef
Sean O'Connor <so@seano.dev>
2021-12-09 13:05:41 -0700

    Add support for TypeScript with Babel

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log("Hello, World!");
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var hello = "Hello, World!";
+console.log(hello);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const hello = "Hello, World!" as string;
+console.log(hello);
+
+export {};
