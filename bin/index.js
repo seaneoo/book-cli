41f837ce4373c1125afc4c837f4457cd2de87407
Sean O'Connor <so@seano.dev>
2021-12-09 13:35:26 -0700

    Testing readline

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,2 @@
 #!/usr/bin/env node
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var hello = "Hello, World!";
-console.log(hello);
+"use strict";var readline=_interopRequireWildcard(require("readline"));function _typeof(a){"@babel/helpers - typeof";return _typeof="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(a){return typeof a}:function(a){return a&&"function"==typeof Symbol&&a.constructor===Symbol&&a!==Symbol.prototype?"symbol":typeof a},_typeof(a)}Object.defineProperty(exports,"__esModule",{value:!0});function _getRequireWildcardCache(a){if("function"!=typeof WeakMap)return null;var b=new WeakMap,c=new WeakMap;return(_getRequireWildcardCache=function(a){return a?c:b})(a)}function _interopRequireWildcard(a,b){if(!b&&a&&a.__esModule)return a;if(null===a||"object"!==_typeof(a)&&"function"!=typeof a)return{default:a};var c=_getRequireWildcardCache(b);if(c&&c.has(a))return c.get(a);var d={},e=Object.defineProperty&&Object.getOwnPropertyDescriptor;for(var f in a)if("default"!=f&&Object.prototype.hasOwnProperty.call(a,f)){var g=e?Object.getOwnPropertyDescriptor(a,f):null;g&&(g.get||g.set)?Object.defineProperty(d,f,g):d[f]=a[f]}return d["default"]=a,c&&c.set(a,d),d}var rl=readline.createInterface({input:process.stdin,output:process.stdout});rl.question("What is your name? ",function(a){console.log("Hello, ".concat(a,"!")),rl.close()}),rl.on("close",function(){process.exit(0)});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 
-const hello = 'Hello, World!' as string
-console.log(hello)
+/**
+ * EXAMPLE USING READLINE
+ * ** NOT FINAL CODE **
+ */
+
+import * as readline from 'readline'
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+rl.question('What is your name? ', (name) => {
+    console.log(`Hello, ${name}!`)
+    rl.close()
+})
+
+rl.on('close', () => {
+    process.exit(0)
+})
 
 export {}
