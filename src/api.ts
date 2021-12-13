10a32acdf1aba609841e8c99e28cc545ff355bc8
Sean O'Connor <so@seano.dev>
2021-12-12 23:17:09 -0700

    Create 'Book' typing

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,13 @@
 import fetch from 'node-fetch'
 const BOOKS_API_KEY = process.env.GKEY
+
+export type Book = {
+    title: string
+    authors: string[]
+    publisher: string
+    publishedDate: string
+    description: string
+    pageCount: number
+    categories: string[]
+}
+
