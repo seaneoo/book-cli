223e828de07a77f887199ee016b69b9b9573e85a
Sean O'Connor <so@seano.dev>
2021-12-12 23:17:32 -0700

    Prompt for ISBN, output results

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import * as readline from 'readline'
+import { search } from './api'
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
-rl.question('What is your name? ', (name) => {
-    console.log(`Hello, ${name}!`)
+rl.question("What is your book's ISBN? ", async (isbn) => {
+    console.log(`Searching for ISBN ${isbn}...`)
+
+    const book = await search(isbn)
+    console.log(book.title)
+    console.log('by ' + book.authors.join(', '))
+    console.log(book.pageCount + ' pages')
+
     rl.close()
 })
 
