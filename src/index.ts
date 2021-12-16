7482609eea9806afa2da3dbf1a7c756dfd9fbd1d
Sean O'Connor <so@seano.dev>
2021-12-16 12:24:57 -0700

    Setup yargs, formatting changes

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
 #!/usr/bin/env node
 
+// TODO: Alternative way to handle providing your own Google API key
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 import * as readline from 'readline'
-import { search } from './api'
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+yargs(hideBin(process.argv)).argv
+
+import * as api from './api'
+
 rl.question("What is your book's ISBN? ", async (isbn) => {
-    console.log(`Searching for ISBN ${isbn}...`)
-
-    const book = await search(isbn)
-    console.log(book.title)
-    console.log('by ' + book.authors.join(', '))
-    console.log(book.pageCount + ' pages')
-
+    await api
+        .search(isbn)
+        .then((book) => {})
+        .catch(console.error)
     rl.close()
 })
 
 rl.on('close', () => {
     process.exit(0)
 })
-
-export {}
