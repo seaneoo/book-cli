c293fae96785944a5472599a373120b8d241cd8c
Sean O'Connor <so@seano.dev>
2021-12-16 12:25:21 -0700

    Compile

--- bin/api.d.ts
+++ bin/api.d.ts
@@ -1,10 +1,17 @@
 export declare type Book = {
-    title: string;
-    authors: string[];
-    publisher: string;
-    publishedDate: string;
-    description: string;
-    pageCount: number;
-    categories: string[];
+    title?: string;
+    subtitle?: string;
+    authors?: string[];
+    publisher?: string;
+    publishedDate?: string;
+    description?: string;
+    industryIdentifiers?: [{
+        type?: string;
+        identifier?: string;
+    }];
+    pageCount?: number;
+    categories?: string[];
+    language?: string;
 };
+export declare const testISBN: (isbn: string) => boolean;
 export declare const search: (isbn: string) => Promise<Book>;
--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,59 +28,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var dotenv = require("dotenv");
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
 dotenv.config();
-var readline = require("readline");
-var api_1 = require("./api");
-var rl = readline.createInterface({
+const readline = __importStar(require("readline"));
+const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
-rl.question("What is your book's ISBN? ", function (isbn) { return __awaiter(void 0, void 0, void 0, function () {
-    var book;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                console.log("Searching for ISBN ".concat(isbn, "..."));
-                return [4 /*yield*/, (0, api_1.search)(isbn)];
-            case 1:
-                book = _a.sent();
-                console.log(book.title);
-                console.log('by ' + book.authors.join(', '));
-                console.log(book.pageCount + ' pages');
-                rl.close();
-                return [2 /*return*/];
-        }
-    });
-}); });
-rl.on('close', function () {
+const yargs_1 = __importDefault(require("yargs"));
+const helpers_1 = require("yargs/helpers");
+(0, yargs_1.default)((0, helpers_1.hideBin)(process.argv)).argv;
+const api = __importStar(require("./api"));
+rl.question("What is your book's ISBN? ", (isbn) => __awaiter(void 0, void 0, void 0, function* () {
+    yield api
+        .search(isbn)
+        .then((book) => { })
+        .catch(console.error);
+    rl.close();
+}));
+rl.on('close', () => {
     process.exit(0);
 });
